187fd5938b803690861a4a0135152dc0aece54cc
= <callixte.girard@isep.fr>
2019-12-10 03:39:12 +0100

    containers turned back to white, but not underground

--- src/static/constants.js
+++ src/static/constants.js
@@ -7,11 +7,11 @@ export const style_dark = { // avoid killing your eyes !
     height: 500,
     width: 500,
 
-    color: "#e7e0e0",
-    background: "#1a1818",
+    // color: "#e7e0e0",
+    // background: "#1a1818",
     font: "comic-sans-ms",
 
-    border: "1px solid black",
+    // border: "1px solid black",
     display: 'inline-block',
     // padding: 40
 }
